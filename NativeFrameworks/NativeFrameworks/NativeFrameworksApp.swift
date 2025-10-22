8ecf49d8e59f01e9ce6d963fcdf023a94143cc44
Micael Martins de Moura <micael.mdemoura@gmail.com>
2025-10-22 12:03:18 -0300

    Initial Commit

---
+++ NativeFrameworks/NativeFrameworks/NativeFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeFrameworksApp.swift
+//  NativeFrameworks
+//
+//  Created by Micael Martins de Moura on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NativeFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
